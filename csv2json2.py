a062400ca3b34758e9c9e70ea5353843408a43e1
Mark Hansen <mark@markhansen.co.nz>
2020-02-11 21:30:10 +1100

    Make a 2-liner

--- csv2json2.py
+++ csv2json2.py
@@ -1,6 +1,3 @@
-import sys
-import csv
-import json
-
 # Note all output fields are strings
-json.dump([line for line in csv.DictReader(sys.stdin)], sys.stdout)
+import sys, csv, json
+json.dump(list(csv.DictReader(sys.stdin)), sys.stdout)
